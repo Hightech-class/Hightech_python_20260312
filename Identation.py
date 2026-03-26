ede9585d36b27af17c4b1008e82181c22fff1087
Hazard10fan <super6257@gmail.com>
2026-03-26 10:51:22 +0900

    Update Identation.py

--- Identation.py
+++ Identation.py
@@ -1,4 +1,4 @@
-# 1. 첫 번째 실습: if문 들여쓰기
+# 1. 첫 번째 실습: 들여쓰기
 def practice_if_indent():
     print("들여쓰기가 잘됐나 확인하기")
     print("들여쓰기가 잘된거 확인했음") 
@@ -6,7 +6,7 @@ def practice_if_indent():
 practice_if_indent()
 
 
-# 2. 두 번째 실습: for문과 break
+# 2. 두 번째 실습: 들여쓰기,for&break
 def practice_for_break():
     print("\n--- 실습 2 시작 ---")
     for i in range(1, 11):
@@ -14,11 +14,3 @@ def practice_for_break():
         if i == 5:
             break
 practice_for_break()    
-
-# --------------------------------------------------------
-# [실행 구역] 
-# 아래 줄을 각 코드의 하단에 써넣으면 해당 실습이 실행됨.
-# --------------------------------------------------------
-practice_if_indent()
-practice_for_break()
-# --------------------------------------------------------
